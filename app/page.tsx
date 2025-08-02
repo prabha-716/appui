e6da1f95264ffb76e1d8fbadedb96b2154b7fda7
prabha-716 <prabakarankaniraman@gmail.com>
2025-08-02 16:26:21 +0530

    search_bar

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,14 @@ export default function PageLayout() {
     <div className="min-h-screen bg-[#FFFDF2] text-[#323232] p-6 flex-row font-sans">
       <div>
         <header className="flex items-center justify-between mb-6">
-        <h1 className="text-6xl font-sans">MOVIE RECOMMENDATION</h1>
+        <h1 className="text-5xl font-sans ">MOVIE RECOMMENDATION</h1>
       </header>
       </div>
       <div className="flex flex-col justify-between gap-7">
-      <div className="h-85 border border-[#c7ae94] ">
-        <p className="px-3.5 py-3.5 text-4xl font-sans">search your movie here..</p>
-              <div className=" px-3.5 py-3.5 ml-20"><SearchBar /></div>
+        
+      <div className="h-70 border border-[#c7ae94] p-6">
+        <p className="font-sans font-medium text-3xl">Search your movie here...</p>
+              <div className="p-6"><SearchBar /></div>
       </div>
       <MovieRow movies={movies}/>
       <div className="border-t border-[#c7ae94] my-6" />
